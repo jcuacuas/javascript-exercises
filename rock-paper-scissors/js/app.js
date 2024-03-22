0454a3a6f0b741425026a0e4ab65a221cb81d49d
Jose <joscuac@gmail.com>
2024-03-22 17:59:47 -0400

    adding files

---
+++ rock-paper-scissors/js/app.js
@@ -0,0 +1,13 @@
+function add7(number) {
+  return x = number + 10
+}
+
+function capitalize(string) {
+  return capilizedWord = string.charAt(0).toUpperCase();
+}
+
+function lastLetter(word) {
+  return grabLetter = word.charAt(word.length - 1);
+}
+
+document.getElementById("testing").innerHTML = lastLetter("chicken");
