8353744959f07eb2135b4791e67437116883031a
Jose <joscuac@gmail.com>
2024-03-23 19:41:24 -0400

    add rock paper scissors game

--- rock-paper-scissors/js/app.js
+++ rock-paper-scissors/js/app.js
@@ -1,13 +1,64 @@
-function add7(number) {
-  return x = number + 10
+const rockPaperScissors = ["rock", "paper", "scissors"];
+let playerSelection = "";
+let computerSelection = "";
+let playerScore = 0;
+let computerScore = 0;
+let roundResults = "";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
-function capitalize(string) {
-  return capilizedWord = string.charAt(0).toUpperCase();
+function getComputerChoice() {
+  return rockPaperScissors[getRandomInt(3)];
 }
 
-function lastLetter(word) {
-  return grabLetter = word.charAt(word.length - 1);
+function playRound(player, computer) {
+  let playerSelectionValue = rockPaperScissors.indexOf(player);
+  let computerSelectionValue = rockPaperScissors.indexOf(computer);
+  if (playerSelectionValue === computerSelectionValue) {
+    return "a tie";
+  } else if (playerSelectionValue === 0 && computerSelectionValue === 2) {
+    playerScore += 1;
+    return "you win";
+  } else if (playerSelectionValue === 2 && computerSelectionValue === 0) {
+    computerScore += 1;
+    return "you lose";
+  } else if (playerSelectionValue > computerSelectionValue) {
+    playerScore += 1;
+    return "you win";
+  } else if (playerSelectionValue < computerSelectionValue) {
+    computerScore += 1;
+    return "you lose";
+  }
 }
 
-document.getElementById("testing").innerHTML = lastLetter("chicken");
+function gameResults() {
+  if (playerScore > computerScore) {
+    return "you are the victor!!!";
+  } else if (playerScore < computerScore) {
+    return "you lost, try again!!!";
+  } else {
+    return "its a draw";
+  }
+}
+
+function playGame() {
+  playerSelection = prompt("Time to play!").toLowerCase().trim();
+  computerSelection = getComputerChoice();
+  roundResults = playRound(playerSelection, computerSelection);
+  console.log("your choice: " + playerSelection);
+  console.log("computer choice: " + computerSelection);
+  console.log("your score: " + playerScore);
+  console.log("opponent score: " + computerScore);
+  return roundResults;
+}
+
+
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(gameResults());
+
